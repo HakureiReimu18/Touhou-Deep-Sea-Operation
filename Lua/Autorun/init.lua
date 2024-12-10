b533b0d02772f797f778f99abf5da34159df4680
HakureiReimu18 <140368777+HakureiReimu18@users.noreply.github.com>
2024-12-11 04:10:16 +0800

    update

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -4,6 +4,7 @@ TLE.Version = "1.0"
 TLE.VersionNum = 01000000
 TLE.Path = table.pack(...)[1]
 
+dofile(TLE.Path.."/Lua/Scripts/Sever/Touhou_Hisoutensoku_Amor.lua")
 
 if CLIENT then
 	Timer.Wait(function()
@@ -11,4 +12,8 @@ if CLIENT then
 		print(runstring)  
 	end,1)
 	dofile(TLE.Path.."/Lua/Scripts/Client/Touhou_Cam_Offset.lua")
+end
+
+if CLIENT then
+	dofile(TLE.Path.."/Lua/Scripts/Client/Alice_Doll_Control_Change_Client.lua")
 end
