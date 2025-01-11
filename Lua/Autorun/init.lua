b74f63c4109007571537675dc7763f5379465e0a
HakureiReimu18 <140368777+HakureiReimu18@users.noreply.github.com>
2025-01-11 16:46:04 +0800

    update

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -4,16 +4,10 @@ TLE.Version = "1.0"
 TLE.VersionNum = 01000000
 TLE.Path = table.pack(...)[1]
 
-dofile(TLE.Path.."/Lua/Scripts/Sever/Touhou_Hisoutensoku_Amor.lua")
-
 if CLIENT then
 	Timer.Wait(function()
 		local runstring = "\n/// Touhou Lua Expansion"..TLE.Version.." ///\n"
 		print(runstring)  
 	end,1)
 	dofile(TLE.Path.."/Lua/Scripts/Client/Touhou_Cam_Offset.lua")
-end
-
-if CLIENT then
-	dofile(TLE.Path.."/Lua/Scripts/Client/Alice_Doll_Control_Change_Client.lua")
 end
