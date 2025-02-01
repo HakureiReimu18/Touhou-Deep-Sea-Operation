cc8ce968d7a4e6f8c04d0700423b694eee8a5f83
HakureiReimu18 <140368777+HakureiReimu18@users.noreply.github.com>
2025-02-01 16:06:00 +0800

    update

--- Lua/Scripts/Client/Touhou_Cam_Offset.lua
+++ Lua/Scripts/Client/Touhou_Cam_Offset.lua
@@ -20,25 +20,19 @@ Hook.Patch("Barotrauma.Character", "ControlLocalPlayer", function(instance)
     --[[slottype中2代表右手，4代表左手]]
     if PlayerInput.SecondaryMouseButtonHeld()
             and (character.HasEquippedItem("Touhou_Cam_Offset_Low",true,2) or character.HasEquippedItem("Touhou_Cam_Offset_Low",true,4))
-            and not character.SelectedItem
-            and not (character.HasEquippedItem("mountableweapon",true,2) or character.HasEquippedItem("mountableweapon",true,4))
-            and not (character.HasEquippedItem("tsm_farsight",true,4) or character.HasEquippedItem("tsm_farsight",true,2)) then
+            and not character.SelectedItem then
         Screen.Selected.Cam.OffsetAmount = math.min(Lerp(Screen.Selected.Cam.OffsetAmount, 0, 0.24)
                 * (1 + math.min( GetSkillLevel(character,"Touhou_Magic") * 0.001, 0.15)), 356.5)
     end
     if PlayerInput.SecondaryMouseButtonHeld()
             and (character.HasEquippedItem("Touhou_Cam_Offset_Normal",true,2) or character.HasEquippedItem("Touhou_Cam_Offset_Normal",true,4))
-            and not character.SelectedItem
-            and not (character.HasEquippedItem("mountableweapon",true,2) or character.HasEquippedItem("mountableweapon",true,4))
-            and not (character.HasEquippedItem("tsm_farsight",true,4) or character.HasEquippedItem("tsm_farsight",true,2)) then
+            and not character.SelectedItem then
         Screen.Selected.Cam.OffsetAmount = math.min(Lerp(Screen.Selected.Cam.OffsetAmount, 0, 0.42)
                 * (1 + math.min( GetSkillLevel(character,"Touhou_Magic") * 0.001, 0.15)), 408.25)
     end
     if PlayerInput.SecondaryMouseButtonHeld()
             and (character.HasEquippedItem("Touhou_Cam_Offset_High",true,2) or character.HasEquippedItem("Touhou_Cam_Offset_High",true,4))
-            and not character.SelectedItem
-            and not (character.HasEquippedItem("mountableweapon",true,2) or character.HasEquippedItem("mountableweapon",true,4))
-            and not (character.HasEquippedItem("tsm_farsight",true,4) or character.HasEquippedItem("tsm_farsight",true,2)) then
+            and not character.SelectedItem then
         Screen.Selected.Cam.OffsetAmount = math.min(Lerp(Screen.Selected.Cam.OffsetAmount, 0, 0.64)
                 * (1 + math.min( GetSkillLevel(character,"Touhou_Magic") * 0.001, 0.15)), 471.5)
         end
