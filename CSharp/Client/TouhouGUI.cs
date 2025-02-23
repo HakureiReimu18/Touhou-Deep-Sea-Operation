e9ad58d213c9a2496588c0be230f7ecb424e5b7f
HakureiReimu18 <140368777+HakureiReimu18@users.noreply.github.com>
2025-02-24 02:13:26 +0800

    update

---
+++ CSharp/Client/TouhouGUI.cs
@@ -0,0 +1,174 @@
+﻿using Barotrauma;
+using Barotrauma.Items.Components;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+
+namespace TouhouGUI
+{
+    public class TouhouGUI : IAssemblyPlugin
+    {
+        public readonly string Name = "TouhouGUI";
+
+        // 删除以下HitHint相关属性
+        // public static float HitHintTimer { get; set; }
+        // public static int HitHintSize { get; set; } = 10;
+        // public static Color HitHintColor { get; set; }
+        // public static int CrosshairDistance { get; set; } = 10;
+
+        public static float IndicatorRadiusStart { get; set; } = 40.0f;
+        public static float IndicatorThickness { get; set; } = 2.0f;
+        public static float IndicatorSectionRad { get; set; } = 2.0f * MathF.PI;
+        public static float IndicatorRotationAngle { get; set; } = 2.0f * MathF.PI;
+
+        public Harmony? harmonyInstance;
+
+        public void Initialize()
+        {
+            LuaCsLogger.Log($"TouhouGUI loading...");
+            harmonyInstance = new Harmony("Touhou.GUI.ThanksEsirprus");
+        }
+
+        public void Dispose()
+        {
+            harmonyInstance?.UnpatchAll();
+            LuaCsLogger.Log("TouhouGUI disposed!");
+        }
+
+        public void OnLoadCompleted()
+        {
+            if (true)
+            {
+                harmonyInstance.PatchAll();
+                LuaCsLogger.Log($"TouhouGUI loaded!");
+            }
+            else
+            {
+                harmonyInstance = null;
+                LuaCsLogger.Log($"TouhouGUI has been disabled because other mod contains its function.");
+            }
+        }
+
+        public void PreInitPatching() { }
+
+        // 删除以下Harmony补丁类：
+        // [HarmonyPatch(typeof(Character), nameof(Character.ApplyAttack))]
+        // public static class ApplyAttackPatch { ... }
+
+        // [HarmonyPatch(typeof(RangedWeapon), nameof(RangedWeapon.UpdateHUDComponentSpecific))]
+        // public static class UpdateHUDComponentSpecificPatch { ... }
+
+        // 修改后的DrawHUD补丁（移除DrawHint调用）
+        [HarmonyPatch(typeof(RangedWeapon), nameof(RangedWeapon.DrawHUD))]
+        static class RangedWeapon_DrawHUD_Patch
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                bool foundInsertionPoint = false;
+
+                for (int i = 0; i < codes.Count; i++)
+                {
+                    // 查找插入点：当代码为 `this.crossHairPosDirtyTimer` 时
+                    if (codes[i].opcode == OpCodes.Ldarg_0 &&
+                        codes[i + 1].opcode == OpCodes.Ldfld &&
+                        codes[i + 1].operand.ToString().Contains("crossHairPosDirtyTimer"))
+                    {
+                        foundInsertionPoint = true;
+                        // 插入调用 DrawIndicator 的代码
+                        var injectCode = new List<CodeInstruction>
+                        {
+                            new CodeInstruction(OpCodes.Ldarg_0),       // 加载 RangedWeapon 实例
+                            new CodeInstruction(OpCodes.Ldarg_1),       // 加载 SpriteBatch 参数
+                            new CodeInstruction(OpCodes.Call, typeof(TouhouGUI).GetMethod("DrawIndicator")) // 调用方法
+                        };
+                        codes.InsertRange(i + 1, injectCode);
+                        break;
+                    }
+                }
+                if (!foundInsertionPoint)
+                    throw new Exception("Failed to find insertion point in IL code!");
+                return codes;
+            }
+        }
+
+        // ============ 修改后的DrawHint方法（移除HitHint逻辑） ============
+        public static void DrawIndicator(RangedWeapon rangedWeapon, SpriteBatch spriteBatch)
+        {
+
+            if (rangedWeapon?.Item == null) return;
+            ItemContainer container = rangedWeapon.Item.GetComponent<ItemContainer>();
+            if (container == null) return;
+            float containedIndicatorState = rangedWeapon.Item.GetComponent<ItemContainer>().GetContainedIndicatorState();
+
+            Color[] gradientColors = new Color[] {
+                GUIStyle.ColorInventoryEmpty,
+                Color.Orange,
+                GUIStyle.ColorInventoryHalf,
+                Color.LimeGreen,
+                GUIStyle.ColorInventoryFull
+            };
+            Color indicatorColor = MultiColorGradientLerp(containedIndicatorState, gradientColors);
+
+/*            // 绘制环形轮廓
+            TouhouGUIExtensions.DrawDonutSectionOutLine(
+                spriteBatch,
+                PlayerInput.MousePosition,
+                new Range<float>(IndicatorRadiusStart - 1.0f, IndicatorRadiusStart + IndicatorThickness + 1.0f),
+                IndicatorSectionRad,
+                Color.Gray,
+                0,
+                IndicatorRotationAngle);
+
+            // 绘制环形填充
+            TouhouGUIExtensions.DrawDonutSection(
+                spriteBatch,
+                PlayerInput.MousePosition,
+                new Range<float>(IndicatorRadiusStart, IndicatorRadiusStart + IndicatorThickness),
+                containedIndicatorState * IndicatorSectionRad,
+                indicatorColor,
+                0,
+                IndicatorRotationAngle + IndicatorSectionRad,
+                false);*/
+
+            // 显示耐久数值
+            float durabilityValue = containedIndicatorState * 100f;
+            Vector2 textPos = new Vector2(
+                PlayerInput.MousePosition.X,
+                PlayerInput.MousePosition.Y + IndicatorRadiusStart + IndicatorThickness + 15f);
+
+            Vector2 textSize = GUIStyle.SmallFont.MeasureString($"{durabilityValue:0.0}%");
+            Vector2 centeredPos = textPos - textSize / 2;
+
+            GUI.DrawString(
+                spriteBatch,
+                centeredPos,
+                $"{durabilityValue:0.0}%",
+                indicatorColor,
+                font: GUIStyle.SubHeadingFont);
+        }
+
+        // 保留多色渐变方法
+        public static Color MultiColorGradientLerp(float t, params Color[] colors)
+        {
+            if (colors.Length == 0) return Color.White;
+            t = MathHelper.Clamp(t, 0f, 1f);
+
+            float segment = 1f / (colors.Length - 1);
+            int index = (int)(t / segment);
+
+            if (index >= colors.Length - 1)
+            {
+                return colors[^1];
+            }
+
+            float localT = (t - index * segment) / segment;
+            return Color.Lerp(colors[index], colors[index + 1], localT);
+        }
+
+    }
+}
---
+++ CSharp/Client/TouhouGUIExtensions.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Immutable;
+using Barotrauma;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Linq;
+using TouhouGUI;
+
+namespace TouhouGUI
+{
+    public static class TouhouGUIExtensions
+    {
+        public const int DonutSegments = 30;
+
+        public static readonly VertexPositionColorTexture[] donutVerts = new VertexPositionColorTexture[DonutSegments * 4];
+
+        public static readonly ImmutableArray<Vector2> canonicalCircle = Enumerable.Range(0, DonutSegments)
+                                                                                   .Select(i => i * (2.0f * MathF.PI / DonutSegments))
+                                                                                   .Select(angle => new Vector2(MathF.Cos(angle), MathF.Sin(angle)))
+                                                                                   .ToImmutableArray();
+
+        private static Vector2 GetDirection(int index, float sectionProportion, int maxDirectionIndex)
+        {
+            int directionIndex = GetDirectionIndex(index);
+            Vector2 direction = canonicalCircle[directionIndex % DonutSegments];
+            if (maxDirectionIndex > 0 && directionIndex >= maxDirectionIndex)
+            {
+                float maxSectionProportion = (float)maxDirectionIndex / DonutSegments;
+                direction = Vector2.Lerp(
+                    canonicalCircle[maxDirectionIndex - 1],
+                    canonicalCircle[maxDirectionIndex % DonutSegments],
+                    1.0f - (maxSectionProportion - sectionProportion) * DonutSegments);
+            }
+
+            return new Vector2(direction.Y, -direction.X);
+        }
+
+        private static int GetDirectionIndex(int index)
+        {
+            return (index % 4) switch
+            {
+                0 => (index / 4) + 0,
+                1 => (index / 4) + 1,
+                2 => (index / 4) + 0,
+                3 => (index / 4) + 1,
+                _ => throw new InvalidOperationException()
+            };
+        }
+
+        private static float GetRadius(int index, ref Range<float> radii)
+        {
+            return (index % 4) switch
+            {
+                0 => radii.End,
+                1 => radii.End,
+                2 => radii.Start,
+                3 => radii.Start,
+                _ => throw new InvalidOperationException()
+            };
+        }
+
+        private static Vector2 RotatePoint(Vector2 center, Vector2 vertexPosition, float rotationAngle)
+        {
+            float cosTheta = MathF.Cos(rotationAngle);
+            float sinTheta = MathF.Sin(rotationAngle);
+
+            float offsetX = vertexPosition.X - center.X;
+            float offsetY = vertexPosition.Y - center.Y;
+
+            float rotatedX = cosTheta * offsetX - sinTheta * offsetY;
+            float rotatedY = sinTheta * offsetX + cosTheta * offsetY;
+
+            return new Vector2(rotatedX + center.X, rotatedY + center.Y);
+        }
+
+        // ============ 修改部分开始 ============
+        public static void DrawDonutSection(SpriteBatch sb, Vector2 center, Range<float> radii, float sectionRad, Color clr, float depth = 0.0f, float rotationAngle = 0.0f, bool clockwise = true)
+        {
+            float sectionProportion = sectionRad / (MathF.PI * 2.0f);
+            int maxDirectionIndex = Math.Min(DonutSegments, (int)MathF.Ceiling(sectionProportion * DonutSegments));
+
+            // 五色渐变定义
+            Color[] gradientColors = new Color[] {
+                GUIStyle.ColorInventoryEmpty,
+                Color.Orange,
+                GUIStyle.ColorInventoryHalf,
+                Color.LimeGreen,
+                GUIStyle.ColorInventoryFull
+            };
+
+            for (int vertexIndex = 0; vertexIndex < maxDirectionIndex * 4; vertexIndex++)
+            {
+                Vector2 direction = GetDirection(vertexIndex, sectionProportion, maxDirectionIndex);
+                Vector2 vertexPosition = new(
+                    center.X + (direction.X * GetRadius(vertexIndex, ref radii)),
+                    center.Y + (direction.Y * GetRadius(vertexIndex, ref radii)));
+
+                Vector2 rotatedPosition = RotatePoint(center, vertexPosition, rotationAngle);
+
+                // 修改顶点颜色计算部分
+                float angle = rotationAngle + (vertexIndex / (float)(maxDirectionIndex * 4)) * sectionRad;
+                float angleProgress = angle / (MathHelper.TwoPi);
+                angleProgress = (angleProgress % 1f + 1f) % 1f; // 确保在0-1范围内
+                Color vertexColor = TouhouGUI.MultiColorGradientLerp(angleProgress, gradientColors);
+
+                donutVerts[vertexIndex].Color = vertexColor;
+                donutVerts[vertexIndex].Position = new Vector3(rotatedPosition, depth);
+            }
+
+            sb.Draw(GUI.WhiteTexture, donutVerts, depth, count: maxDirectionIndex);
+        }
+        // ============ 修改部分结束 ============
+
+        public static void DrawDonutSectionOutLine(SpriteBatch sb, Vector2 center, Range<float> radii, float sectionRad, Color outlineclr, float depth = 0.0f, float rotationAngle = 0.0f)
+        {
+            float outlineWidth = 1.0f;
+            //draw curved line
+            DrawDonutSection(sb, center, new Range<float>(radii.Start - outlineWidth, radii.Start), sectionRad, outlineclr, depth, rotationAngle);
+            DrawDonutSection(sb, center, new Range<float>(radii.End, radii.End + outlineWidth), sectionRad, outlineclr, depth, rotationAngle);
+            //draw straight line
+            Vector2 start1 = new(center.X + (radii.Start - outlineWidth) * MathF.Sin(rotationAngle), center.Y + (radii.Start - outlineWidth) * MathF.Cos(rotationAngle));
+            Vector2 end1 = new(center.X + (radii.End + outlineWidth) * MathF.Sin(rotationAngle), center.Y + (radii.End + outlineWidth) * MathF.Cos(rotationAngle));
+            GUI.DrawLine(sb, start1, end1, outlineclr, depth);
+            Vector2 start2 = new(center.X + (radii.Start - outlineWidth) * MathF.Sin(rotationAngle + sectionRad), center.Y + (radii.Start - outlineWidth) * MathF.Cos(rotationAngle + sectionRad));
+            Vector2 end2 = new(center.X + (radii.End + outlineWidth) * MathF.Sin(rotationAngle + sectionRad), center.Y + (radii.End + outlineWidth) * MathF.Cos(rotationAngle + sectionRad));
+            GUI.DrawLine(sb, start2, end2, outlineclr, depth);
+        }
+    }
+}
